a1a98cca823587b59aec889cea97e7aca6a7bdea
ska034 <al.ig.erofeev@mail.ru>
2021-01-26 00:31:52 +0300

    Task 3.1 - task 3.3

--- task3/src/eu/senla/task3/Cat.java
+++ task3/src/eu/senla/task3/Cat.java
@@ -1,16 +1,62 @@
 package eu.senla.task3;
 
-//Task 1
+
 
 public class Cat {
-    private final String  CAT_NAME= "Jarvis";
-    private final int CAT_AGE = 3;
 
-    public String getCAT_NAME() {
-        return CAT_NAME;
+//Task1
+//        private final String  CAT_NAME= "Jarvis";
+//        private final int CAT_AGE = 3;
+
+//    public void Mycat() {
+//        int i = 1;
+//        while (i <= 10){
+//            System.out.println(i + ". Name: " + CAT_NAME + " Age: " + CAT_AGE);
+//            i++;
+//        }
+//    }
+
+//Task2
+//    private String  CAT_NAME= "Jarvis";
+//    private int CAT_AGE = 3;
+//
+//    public String getCAT_NAME() {
+//        return CAT_NAME;
+//    }
+//
+//    public int getCAT_AGE() {
+//        return CAT_AGE;
+//    }
+//
+//    public void setCAT_NAME(String CAT_NAME) {
+//        this.CAT_NAME = CAT_NAME;
+//    }
+//
+//    public void setCAT_AGE(int CAT_AGE) {
+//        this.CAT_AGE = CAT_AGE;
+//    }
+
+//Task3
+    private String name;
+    private int age;
+    public Cat(String name, int age){
+        this.name = name;
+        this.age = age;
     }
 
-    public int getCAT_AGE() {
-        return CAT_AGE;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
--- task3/src/eu/senla/task3/Main.java
+++ task3/src/eu/senla/task3/Main.java
@@ -4,13 +4,27 @@ public class Main {
     public static void main(String[] args) {
 
 //Task1
+//        Cat cat = new Cat();
+//        cat.Mycat();
 
-        int i = 1;
-        while (i <= 10){
-            Cat cat = new Cat();
-            System.out.println(i + ". Name: " + cat.getCAT_NAME() + " Age: " + cat.getCAT_AGE());
+
+//Task2
+//        for (int i = 1; i<=10; i++){
+//            Cat cat = new Cat();
+//            cat.setCAT_NAME("Mark");
+//            cat.setCAT_AGE(1);
+//            System.out.println(i + ". Name: " + cat.getCAT_NAME() + " Age: " + cat.getCAT_AGE());
+//        }
+
+
+//Task3
+    int i = 1;
+    Cat MarkII = new Cat("MARK_II",7);
+
+        do {
+            System.out.println(i + ".Name: " + MarkII.getName() + "     Age: " + MarkII.getAge());
             i++;
-        }
+        } while (i<=10);
 
     }
 }
