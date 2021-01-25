91158fdfff356a59758ca32b510193afae85f7ed
ska034 <al.ig.erofeev@mail.ru>
2021-01-25 23:11:42 +0300

    Task 3.1

---
+++ task3/src/eu/senla/task3/Cat.java
@@ -0,0 +1,16 @@
+package eu.senla.task3;
+
+//Task 1
+
+public class Cat {
+    private final String  CAT_NAME= "Jarvis";
+    private final int CAT_AGE = 3;
+
+    public String getCAT_NAME() {
+        return CAT_NAME;
+    }
+
+    public int getCAT_AGE() {
+        return CAT_AGE;
+    }
+}
--- task3/src/eu/senla/task3/Main.java
+++ task3/src/eu/senla/task3/Main.java
@@ -2,6 +2,15 @@ package eu.senla.task3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("323123");
+
+//Task1
+
+        int i = 1;
+        while (i <= 10){
+            Cat cat = new Cat();
+            System.out.println(i + ". Name: " + cat.getCAT_NAME() + " Age: " + cat.getCAT_AGE());
+            i++;
+        }
+
     }
 }
