2c77a8f1a48752664f604165a0830b7c1e369c20
ska034 <al.ig.erofeev@mail.ru>
2021-01-29 01:26:21 +0300

    New Task4.2

--- Task4/src/eu/senla/task4/Main.java
+++ Task4/src/eu/senla/task4/Main.java
@@ -2,26 +2,80 @@ package eu.senla.task4;
 
 public class Main {
     public static void main(String[] args) {
-        int [][] array= new int [10][10];
-        int a = 1;
+        final String letters = "ABCDEFGHIKLMNOPQRSTVXYZ";
+        int letterslength = letters.length();
+        String [][] array = new String[10][10];
+        int count = 0;
 
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                if ((i == j)|| (j == array.length-a)){
-                    array[i][j] = 1;
+
+                String lettersrandom ="";
+                for (int k = 0; k < 6; k++) {
+                    int random = (int) (Math.random()*23);
+                    lettersrandom += (letters.charAt(random));
+                }
+
+                int DoubleOrString = (int) (Math.random()*2);
+                if (count % 3 == 0){
+                    array[i][j] = (String.valueOf(Math.random()*10)).substring(0,6);
                 }
                 else {
-                    array[i][j] = 0;
+                    array[i][j] = lettersrandom;
                 }
+                count++;
+
             }
-            a++;
         }
+
+
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
+
+
+
+
+
+
+//        System.out.println(lettersrandom);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        int [][] array= new int [10][10];
+//        int a = 1;
+//
+//
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array[i].length; j++) {
+//                if ((i == j)|| (j == array.length-a)){
+//                    array[i][j] = 1;
+//                }
+//                else {
+//                    array[i][j] = 0;
+//                }
+//            }
+//            a++;
+//        }
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array[i].length; j++) {
+//                System.out.print(array[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
     }
 }
