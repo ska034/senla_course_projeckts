273b7cdc9818c4e2fb925f5a60c29a2838920948
ska034 <al.ig.erofeev@mail.ru>
2021-01-29 01:34:24 +0300

    add Task4.2

--- Task4/src/eu/senla/task4/Arrays.java
+++ Task4/src/eu/senla/task4/Arrays.java
@@ -1,4 +1,65 @@
 package eu.senla.task4;
 
 public class Arrays {
+    public static void firstarray(){
+        final String letters = "ABCDEFGHIKLMNOPQRSTVXYZ";
+        int letterslength = letters.length();
+        String [][] array = new String[10][10];
+        int count = 0;
+
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+
+                String lettersrandom ="";
+                for (int k = 0; k < 6; k++) {
+                    int random = (int) (Math.random()*23);
+                    lettersrandom += (letters.charAt(random));
+                }
+
+                int DoubleOrString = (int) (Math.random()*2);
+                if (count % 3 == 0){
+                    array[i][j] = (String.valueOf(Math.random()*10)).substring(0,6);
+                }
+                else {
+                    array[i][j] = lettersrandom;
+                }
+                count++;
+
+            }
+        }
+
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void secondarray(){
+        int [][] array= new int [10][10];
+        int a = 1;
+
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if ((i == j)|| (j == array.length-a)){
+                    array[i][j] = 1;
+                }
+                else {
+                    array[i][j] = 0;
+                }
+            }
+            a++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
