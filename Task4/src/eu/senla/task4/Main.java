e5857b8ec0c8aab5ca3650ee00e73e367a2b4eca
ska034 <al.ig.erofeev@mail.ru>
2021-01-29 01:38:54 +0300

    add

--- Task4/src/eu/senla/task4/Main.java
+++ Task4/src/eu/senla/task4/Main.java
@@ -2,7 +2,7 @@ package eu.senla.task4;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println();
         Arrays.firstarray();
 
 //        System.out.println();
