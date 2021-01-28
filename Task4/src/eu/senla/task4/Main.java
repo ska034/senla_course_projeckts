9fe7be2a871c2a92525a880f4c5e78c287d9739e
ska034 <al.ig.erofeev@mail.ru>
2021-01-28 22:37:31 +0300

    New Task4.1

--- Task4/src/eu/senla/task4/Main.java
+++ Task4/src/eu/senla/task4/Main.java
@@ -1,4 +1,27 @@
 package eu.senla.task4;
 
 public class Main {
+    public static void main(String[] args) {
+        int [][] array= new int [10][10];
+        int a = 1;
+
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if ((i == j)|| (j == array.length-a)){
+                    array[i][j] = 1;
+                }
+                else {
+                    array[i][j] = 0;
+                }
+            }
+            a++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
