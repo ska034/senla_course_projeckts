6c0a5d93060447ea2ecd888285ae7c3dc79f208e
ska034 <al.ig.erofeev@mail.ru>
2021-01-31 17:02:26 +0300

    add Task4.3

--- Task4/src/eu/senla/task4/Arrays.java
+++ Task4/src/eu/senla/task4/Arrays.java
@@ -7,7 +7,10 @@ public class Arrays {
         String [][] array = new String[10][10];
         int count = 0;
 
+        String [] diagonal_main = new String[10];
+        String [] diagonal_side = new String[10];
 
+// Инициализируем основную матрицу
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
 
@@ -29,15 +32,35 @@ public class Arrays {
             }
         }
 
-
-        for (int i = 0; i < array.length; i++) {
+// Выводим основную матрицу, и инициализируем массивы из диагоналей
+        for (int i = 0, k =0, l = 0, m = 1; i < array.length; i++, k++,l++,m++) {
             for (int j = 0; j < array[i].length; j++) {
+                if (i == j){
+                    diagonal_main[k] = array[i][j];
+                }
+                if (j == array.length - m){
+                    diagonal_side[l] = array[i][j];
+                }
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
 
+// Выводим диагонали
+        System.out.println();
+        System.out.print("diagonal_main: ");
+        for (String x:diagonal_main) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        System.out.print("diagonal_side: ");
+        for (String x:diagonal_side) {
+            System.out.print(x + " ");
+        }
+
     }
+
+
     public static void secondarray(){
         int [][] array= new int [10][10];
         int a = 1;
@@ -55,7 +78,7 @@ public class Arrays {
             a++;
         }
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
+            for (int j = 0; j <  array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
